9d34abfc08e30fbfffd6b55afda5cbf5849fb518
beatarjn <beatarejman89@gmail.com>
2024-11-11 19:38:43 +0100

    209. Minium Size Subarray Sum (leetcode) - add solution

--- src/test/java/leetcode/MinimumSizeSubarraySumTest.java
+++ src/test/java/leetcode/MinimumSizeSubarraySumTest.java
@@ -24,7 +24,7 @@ class MinimumSizeSubarraySumTest {
                 Arguments.of(new int[]{2, 3, 1, 2, 4, 3}, 7, 2),
                 Arguments.of(new int[]{1, 4, 4}, 4, 1),
                 Arguments.of(new int[]{1, 1, 1, 1, 1, 1, 1, 1}, 11, 0),
-                Arguments.of(new int[]{2, 16, 14, 15}, 20, 0),
+                Arguments.of(new int[]{2, 16, 14, 15}, 20, 2),
                 Arguments.of(new int[]{1, 2, 3, 4, 5}, 9, 2),
                 Arguments.of(new int[]{1, 2, 3, 4, 5}, 11, 3)
         );
